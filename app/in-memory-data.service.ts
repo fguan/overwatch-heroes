986e3cb32b099feae2b58c750f2ffbf2b0c055ce
fguan <fguan@dangermind.com>
2016-07-15 18:10:29 -0700

    add heroes

--- app/in-memory-data.service.ts
+++ app/in-memory-data.service.ts
@@ -1,16 +1,29 @@
 export class InMemoryDataService {
     createDb() {
         let heroes = [
-            { id: 11, name: 'Mr. Nice' },
-            { id: 12, name: 'Narco' },
-            { id: 13, name: 'Bombasto' },
-            { id: 14, name: 'Celeritas' },
-            { id: 15, name: 'Magneta' },
-            { id: 16, name: 'RubberMan' },
-            { id: 17, name: 'Dynama' },
-            { id: 18, name: 'Dr IQ' },
-            { id: 19, name: 'Magma' },
-            { id: 20, name: 'Tornado' }
+            { id: 11, name: 'Genji' },
+            { id: 12, name: 'McCree' },
+            { id: 13, name: 'Pharah' },
+            { id: 14, name: 'Reaper' },
+            { id: 15, name: 'Soldier: 76' },
+            { id: 16, name: 'Tracer' },
+            { id: 17, name: 'Bastion' },
+            { id: 18, name: 'Hanzo' },
+            { id: 19, name: 'Junkrat' },
+            { id: 20, name: 'Mei' },
+            { id: 21, name: 'Torbjörn' },
+            { id: 22, name: 'Widowmaker' },
+            { id: 23, name: 'D.Va' },
+            { id: 24, name: 'Reinhardt' },
+            { id: 25, name: 'Roadhog' },
+            { id: 26, name: 'Winston' },
+            { id: 27, name: 'Zarya' },
+            { id: 28, name: 'Ana' },
+            { id: 29, name: 'Lúcio' },
+            { id: 30, name: 'Mercy' },
+            { id: 31, name: 'Symmetra' },
+            { id: 32, name: 'Zenyatta' },
+
         ];
         return { heroes };
     }
